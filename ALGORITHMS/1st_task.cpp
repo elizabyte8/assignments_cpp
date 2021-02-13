7c31960b2775b26e2ae29633ca71392e10c071b1
cold_summer <73346910+ellyzabe8@users.noreply.github.com>
2021-02-13 19:27:38 -0100

    archer, aim, competition

--- ALGORITHMS/1st_task.cpp
+++ ALGORITHMS/1st_task.cpp
@@ -1,13 +1,9 @@
 
 // ---------- 1st) TASK ---------- archer, aim, competition
-
-
 #include <iostream>
 #include <ctime>
 
-using namespace std;
-
-int main() //MAIN
+int main()// START of MAIN
 {
   srand(time(NULL));
   int random_bullet = rand()%101, aim = 100, 
@@ -15,32 +11,30 @@ int main() //MAIN
 
 cout << "A sniper starts shoting... ->\n" << endl;
 
-  while (succes != 3 && defeat != 2)
+ while (succes != 3 && defeat != 2)
  {
-    if (random_bullet > 0 && random_bullet < 77)
+   if (random_bullet > 0 && random_bullet < 77)
    {
-     cout <<"Sniper missed!" << endl;
-   defeat++;
-    }
+    std::cout <<"Sniper missed!" << std::endl;
+     defeat++;
+   }
 
-    if (random_bullet > 70 && random_bullet < 100)
+   if (random_bullet > 70 && random_bullet < 100)
    {
-     cout <<"Sniper missed! BUT he was VERY close! " << endl;
-   defeat++;
+    std::cout <<"Sniper missed! BUT he was VERY close! " << std::endl;
+     defeat++;
    }
 
    else
-  {
-    cout <<"Wow! Sniper shoted at the aim! He succeed! " << endl;
-   succes++;
-  }
+   {
+    std::cout <<"Wow! Sniper shoted at the aim! He succeed! " << std::endl;
+     succes++;
+   }
 
    if (defeat == 2)
-    cout << "\nUnfortunately, the sniper defeated 2 times. He lost the competition... " << endl;
+    std::cout << "\nUnfortunately, the sniper defeated 2 times. He lost the competition... " << std::endl;
    if (succes == 3)
-    cout << "\n!OMG! The sniper won the competition! He is the winner!" << endl;
+    std::cout << "\n!OMG! The sniper won the competition! He is the winner!" << std::endl;
  }
-
-
 return 0; 
-} // END
+}// END of MAIN
