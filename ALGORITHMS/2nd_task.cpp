75e5aae5898711f7553716a24aa758888e25766f
cold_summer <73346910+ellyzabe8@users.noreply.github.com>
2021-02-13 20:00:42 -0100

    winds direction, ship, island X

--- ALGORITHMS/2nd_task.cpp
+++ ALGORITHMS/2nd_task.cpp
@@ -1,24 +1,21 @@
 // ---------- 2nd) TASK ---------- winds direction, ship, island X
-
 #include <iostream>
 #include <ctime>
 
-using namespace std;
-
-int main() // MAIN
+int main()// START of MAIN
 {
-
  srand(time(NULL));
  int days = 1, good_condition = 0;
 
  while (good_condition != 5)
-{
+ {
   int air = rand()%4;
-  cout << "\n\n~~~ It is a " << days << " day of our trip. ~~~";
-  cout << "\nWind Direction: ";
+  
+  std::cout << "\n\n~~~ It is a " << days << " day of our trip. ~~~";
+  std::cout << "\nWind Direction: ";
 
   switch(air)
- {
+  {
    case 0:
    cout << "West"; 
    break;
@@ -31,26 +28,24 @@ int main() // MAIN
    case 3:
    cout <<"South";
    break;
- }
-
-  if (air == 2 || air == 1)
- {
-   days++;
-   good_condition++;
-   cout << "\nA good day! We are lucky.";
   }
 
- else
- {
+  if (air == 2 || air == 1)
+  {
    days++;
-   cout << "\nWind conditions are against us.\nThe ship did not move towards its destination today..." << endl;
+   good_condition++;
+   std::cout << "\nA good day! We are lucky.";
+  }
+
+  else
+  {
+   days++;
+   std::cout << "\nWind conditions are against us.\nThe ship did not move towards its destination today..." << std::endl;
   }
 
   if (good_condition == 5)
-  cout << "\n\n...EVENTUALLY we arrived! The ship reached the destination in " << days << " days!...";
-
- }
-
+  std::cout << "\n\n...EVENTUALLY we arrived! The ship reached the destination in " << days << " days!...";
+ }// END of main while loop
 return 0;
-} // END
+}// END of MAIN
 
