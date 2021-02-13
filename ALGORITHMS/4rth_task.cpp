5aaed51f5d44ed53e67f85586c1cac94c5e4888f
cold_summer <73346910+ellyzabe8@users.noreply.github.com>
2021-02-13 20:14:53 -0100

    bees (males and females) and honey

--- ALGORITHMS/4rth_task.cpp
+++ ALGORITHMS/4rth_task.cpp
@@ -1,14 +1,9 @@
 // ---------- 4th) TASK -------- bees (males and females) and honey
-
 #include <iostream>
 #include <cstring>
 #include <ctime>
 
-
-using namespace std;
-
-
-class bee // CLASS
+class bee// START of CLASS
 {
  public:
 
@@ -21,7 +16,7 @@ class bee // CLASS
  int wings;
 
  bee () // constructor of bee class
-{
+ {
   head = "head";
   thorax = "thorax";
   abdomen = "abdomen";
@@ -32,44 +27,45 @@ class bee // CLASS
   wings = 2;
  }
 
-  class female // CLASS
- {
+  class female// START of CLASS
+  {
    public:
 
     int stinger;
     string glands;
 
-     female () // constructor of female class
+     female () // START of constructor
      {
       stinger = 1;
       glands = "many glands";
      }
 
-     int get_stinger() // getter
+     int get_stinger()// START of getter
      {
        return stinger;
      }
 
-     string get_glands() // getter
+     string get_glands()// START of getter
      {
        return glands;
-      }
-    }; // END OF FEMALE CLASS
+     }
+   };// END OF FEMALE CLASS
 
-  void print(bool sex, female f) // METHOD
+  void print(bool sex, female f)// START of method
   {
-    cout << "\nOne " << head <<"\nOne " << thorax << "\nOne " << abdomen << "." << endl;
+    std::cout << "\nOne " << head <<"\nOne " << thorax << "\nOne " << abdomen << "." << std::endl;
 
-    cout << 2 << compound_eye << " compound eyes, " << small_eyes << " small eyes, " << antennae << " antennaes, " << wings << " wings.\n";
+    std::cout << 2 << compound_eye << " compound eyes, " << small_eyes << " small eyes, " << antennae << " antennaes, " << wings << " wings.\n";
 
     if (sex == 0)
-    cout << "ANY MALE DOES NOT possess stinger and glands. It cannot produce honey!" << endl;
+    std::cout << "ANY MALE DOES NOT possess stinger and glands. It cannot produce honey!" << std::endl;
     else
-    cout << "EVERY FEMALE HAS " << f.get_stinger() << " stinger and " << f.get_glands() << "." <<endl;
-   }
-}; // END OF BEE CLASS
+    std::cout << "EVERY FEMALE HAS " << f.get_stinger() << " stinger and " << f.get_glands() << "." << std::endl;
+   }// END of method
+};// END OF BEE CLASS
 
-int main() // MAIN
+
+int main()// START of MAIN
 {
  srand(time(NULL));
  int random_females = rand()%50+41, random_males = rand()%25+26, i = 0;
@@ -79,24 +75,25 @@ int main() // MAIN
  bee male[50];
  bee::female f;
 
-cout << "\n\n~ The program is going to describe parts of a female or a male bee. ~\n\n [...Let's begin with female bees...]\n_We have them in a quantity of: " << random_females << "_" << endl;
-  while (i < random_females)
+std::cout << "\n\n~ The program is going to describe parts of a female or a male bee. ~\n\n [...Let's begin with female bees...]\n_We have them in a quantity of: " << random_females << "_" << std::endl;
+ while (i < random_females)
  {
-   female[i].print(1,f);
+  female[i].print(1,f);
    i++;
-  }
+ }
 
  random_males = rand()%50+41, i = 0;
-cout << "\n\n[...NOW The program is going to describe parts of male bees...]\n_We have them in a quantity of " << random_males << "_" << endl;
-  while (i < random_males)
+std::cout << "\n\n[...NOW The program is going to describe parts of male bees...]\n_We have them in a quantity of " << random_males << "_" << std::endl;
+ while (i < random_males)
  {
-   male[i].print(0,f);
+  male[i].print(0,f);
    i++;
-}
+ }
+ 
  if (random_females >= 70)
- cout << "\nThere are " << random_females << " bees and they ARE ENOUGH to produce honey! :-) ";
+ std::cout << "\nThere are " << random_females << " bees and they ARE ENOUGH to produce honey! :-) ";
  else
- cout << "\nThere are " << random_females << " bees and they ARE NOT ENOUGH to produce honey... :-(... T_T";
+ std::cout << "\nThere are " << random_females << " bees and they ARE NOT ENOUGH to produce honey... :-(... T_T";
 
 return 0;
-} // END
+}// END of MAIN
