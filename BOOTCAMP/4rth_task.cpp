3b38b1b85011207d41d317ed92b1ccc8f6859ac2
cold_summer <73346910+ellyzabe8@users.noreply.github.com>
2021-02-13 20:43:39 -0100

    convert a decimal number to hexadecimal number

--- BOOTCAMP/4rth_task.cpp
+++ BOOTCAMP/4rth_task.cpp
@@ -1,12 +1,10 @@
 
 // ---------- 4th) TASK ----------  convert a decimal number to hexadecimal number 
-
 #include<iostream> 
 #include <cctype>
 
-using namespace std; 
 
-void decimal_to_hexadecimal(int number) // FUNCTION 
+void decimal_to_hexadecimal(int number)// START of func
 { 
  // char array to store hexadecimal number 
  char hexaDeciNum[100]; 
@@ -14,49 +12,48 @@ void decimal_to_hexadecimal(int number) // FUNCTION
  int i = 0; 
  while (number!=0) 
  { 
- // temporary variable to store remainder 
- int temp = 0; 
- // storing remainder in temp variable. 
- temp = number % 16; 
- // check if temp < 10 
- if (temp < 10) 
+  // temporary variable to store remainder 
+  int temp = 0; 
+  // storing remainder in temp variable. 
+  temp = number % 16; 
+  // check if temp < 10 
+  if (temp < 10) 
   { 
     hexaDeciNum[i] = temp + 48; 
      i++; 
   } 
- else
+  else
   { 
     hexaDeciNum[i] = temp + 55; 
      i++; 
   } 
- number = number/16; 
- } 
-cout << "Your decimal number looks like ";
-// printing hexadecimal number array in reverse order 
+  number = number/16; 
+ }// END of while loop 
+std::cout << "Your decimal number looks like ";
+ // printing hexadecimal number array in reverse order 
  for (int j = i-1; j >= 0; j--) 
  {
-   cout << hexaDeciNum[j]; 
+   std::cout << hexaDeciNum[j]; 
  }
-cout << " in hexacedimal numeric system.";
-} // END
+std::cout << " in hexacedimal numeric system.";
+}// END of func
 
 
- 
-int main() // MAIN
+int main()// START of MAIN
 { 
-int number; bool check;
+ int number; bool check;
 
-cout << "This program will help you to convert any decimal number to its hexadecimal form. WARNING! Your number must include digits in the range of between three digits up to six digits.\nPlease, enter the desired number to convert: " << endl;
+ std::cout << "This program will help you to convert any decimal number to its hexadecimal form. WARNING! Your number must include digits in the range of between three digits up to six digits.\nPlease, enter the desired number to convert: " << std::endl;
 
-do
-{
-cin >> number;
-if (isdigit(number) == 0)
-check = false;
-else
-check = true;
-} while ((number < 100 && number > 99999) || check == false);
+ do
+ {
+  std::cin >> number;
+  if (isdigit(number) == 0)
+   check = false;
+  else
+   check = true;
+ } while ((number < 100 && number > 99999) || check == false);
 
-decimal_to_hexadecimal(number); 
+ decimal_to_hexadecimal(number); 
 return 0; 
-} // END
+}// END of MAIN
