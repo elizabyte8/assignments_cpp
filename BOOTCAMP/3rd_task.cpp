151682ee15b96d0fcd18f291bb8616549ba0bb42
cold_summer <73346910+ellyzabe8@users.noreply.github.com>
2021-02-13 20:56:05 -0100

    convert decimal to binary or binary to decimal

--- BOOTCAMP/3rd_task.cpp
+++ BOOTCAMP/3rd_task.cpp
@@ -1,28 +1,27 @@
 // ---------- 3rd) TASK ---------- convert decimal to binary or binary to decimal
-
 #include <iostream> 
 #include <cstring>
 #include <cctype>
 #include <cmath>
 
-using namespace std;
 
-void decimal_to_binary(int number, int arr[], int *size)  // FUNCTION 
+void decimal_to_binary(int number, int arr[], int *size)// START of func
 { 
  int i = 0;
   while(true) 
- {
-    if (number <= 0) 
-     break;
+  {
+   if (number <= 0) 
+    break;
 
- arr[i] = number%2; 
- number = number/2;
- *size += 1; 
- i++;
- } 
-}  // END
+   arr[i] = number%2; 
+   number = number/2;
+   *size += 1; 
+    i++;
+  } 
+}// END of func
 
-int binary_to_decimal(int number)  // FUNCTION
+
+int binary_to_decimal(int number)// START of func
 {
  int i = 0, remainder, sum;
 
@@ -34,56 +33,57 @@ int binary_to_decimal(int number)  // FUNCTION
    remainder = number%10;
    sum = sum + (remainder*pow(2,i));
    number = number/10;
-   i++;
-  }
-
+    i++;
+ }
 return sum;
-}  // END
+}// END of func
 
 
-int main()  // MAIN
+int main()// START of MAIN
 { 
  char choice; int number, binary[50], size = 0;
  bool check = false;
 
- cout << "\nThis program will help you to convert the given decimal number to binary  numeral system\n\tor\n binary number to decimal numeral system!\n\n TAP [D] if you want to convert binary number to decimal numeral system\n\n\tor\n\nTAP [B] if you want to convert decimal number to binary numeral system.\n \nPlease, enter your choice: " <<  endl;
+ std::cout << "\nThis program will help you to convert the given decimal number to binary  numeral system\n\tor\n binary number to decimal numeral system!\n\n TAP [D] if you want to convert binary number to decimal numeral system\n\n\tor\n\nTAP [B] if you want to convert decimal number to binary numeral system.\n \nPlease, enter your choice: " << std::endl;
 
-   do
+  do
  {
-   cin >> choice;
-   choice = toupper(choice);
-   if (choice != 'D' && choice != 'B')
-   cout << "Please, try again: " << endl;
+  std::cin >> choice;
+  choice = toupper(choice);
+  if (choice != 'D' && choice != 'B')
+   std::cout << "Please, try again: " << std::endl;
  } while (choice != 'D' && choice != 'B');
 
- cout << "\nVery well!\nNow enter the desired number to convert and the program will make it for you. \nWarning! Your number must consist MINIMUM 3 digits or MAXIMUM 6 digits: \n"; 
+ std::cout << "\nVery well!\nNow enter the desired number to convert and the program will make it for you. \nWarning! Your number must consist MINIMUM 3 digits or MAXIMUM 6 digits: \n"; 
 
-   do
+  do
  {  
-   cin >> number;
+   std::cin >> number;
    if (isdigit(number) == 1 || (number >= 100 && number <= 999999))
-   check = true; 
+    check = true; 
    else
-   cout <<" Please, try again: " << endl;
+    std::cout <<" Please, try again: " << std::endl;
  } while (check == false);
 
   if (choice == 'D' && (number%10 == 0 && number%2 == 0 ))
-  cout << "Your number is binary! Its decimal form looks like " <<        binary_to_decimal(number);
+   std::cout << "Your number is binary! Its decimal form looks like " << binary_to_decimal(number);
  
  else 
  { 
-  cout << "Your number is decimal! Its binary form looks like ";
+   std::cout << "Your number is decimal! Its binary form looks like ";
   decimal_to_binary(number,binary,&size);
  }
+ 
  for (int i = size-1; i >= 0 ; i--)
- { cout << (binary[i]); }
+  std::cout << (binary[i]); 
 
-cout << "\nDo you wish to continue? Y/n" << endl;
-cin >> choice;
-if (choice == 'Y' || choice == 'y')
-return main();
-else
-cout << "Thank you for using my program! Goodbye ~" << endl;
+ std::cout << "\nDo you wish to continue? Y/n" << std::endl;
+ std::cin >> choice;
+ 
+ if (choice == 'Y' || choice == 'y')
+  return main();
+ else
+  std::cout << "Thank you for using my program! Goodbye ~" << std::endl;
 return 0;
-} // END
+}// END of MAIN
 
