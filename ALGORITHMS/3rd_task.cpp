175e83ee274c938a579182a82b7d7a0cc48afea1
cold_summer <73346910+ellyzabe8@users.noreply.github.com>
2021-02-14 09:05:18 -0100

    bus, passangers, stations, summer 2018

--- ALGORITHMS/3rd_task.cpp
+++ ALGORITHMS/3rd_task.cpp
@@ -27,12 +27,12 @@ int main()// START of MAIN
 
            double_passangers_for_another_if = random_passangers;
 
-           std::cout <<"\n\nIt is " << week[i] << "\nThe bus went through the \n" << stations [j]  << "_station and it took " << random_passangers << " passangers."; 
+           std::cout <<"\n\nIt is " << week[i] << "\nThe bus went through the \n" << stations [j] << "_station and it took " << random_passangers << " passangers."; 
           }
 
          if (i == 4 && j == 0)
          {
-           std::cout <<"\n\nIt is " << week[i]<<" today.\n The bus went through the\n" << stations[j] <<" _station and it took 7 passangers."; 
+           std::cout << "\n\nIt is " << week[i] << " today.\n The bus went through the\n" << stations[j] << " _station and it took 7 passangers."; 
            people = 7;
            total_people += people;
 
@@ -44,45 +44,45 @@ int main()// START of MAIN
           random_passangers = rand()%2+6;
           total_people += random_passangers;
 
-          std::cout <<"\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j]<<"_station and it took "<< random_passangers << " passangers"; 
+          std::cout << "\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j] << "_station and it took " << random_passangers << " passangers"; 
          }
 
          if ((i == 1 && i == 5) && j == 1)
          {
           people = double_passangers_for_another_if*2; 
           total_people += people;
-          std::cout <<"\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j]<<" _station and it took "<< people << " passangers";
+          std::cout << "\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j] << " _station and it took " << people << " passangers";
          }
 
          if (i == 6 && j == 2)
          { 
           people = 6;
           total_people += people;
-          std::cout <<"\n\nIt is " << week[i] << "\nThe bus went through the\n" <<   stations[j]<<"_station and it took "<< people << " passangers"; 
+          std::cout << "\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j] << "_station and it took " << people << " passangers"; 
          }
 
          if (i == 0 && j == 2)
          {
           people = 0;
-          std::cout <<"\n\nIt is " << week[i] << "\nThe bus went through the\n" <<   stations[j]<<"_station and it took "<< people << " passangers because\nTHIS STATION DOES NOT WORK ON SUNDAYS!"; 
+          std::cout << "\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j] << "_station and it took " << people << " passangers because\nTHIS STATION DOES NOT WORK ON SUNDAYS!"; 
          }
 
          if ((i != 0 && i != 6) && j == 2)
          { 
           random_passangers = rand()%5+7;
           total_people += random_passangers;
-          std::cout <<"\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j]<<"_station and it took "<< random_passangers << " passangers"; 
+          std::cout << "\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j] << "_station and it took " << random_passangers << " passangers"; 
          }
 
          if (j == 3)
          {
           people = 0; 
-          std::cout <<"\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j]<<"_station and it took "<< people << " passangers because it is a TERMINAL STATION."; 
+          std::cout << "\n\nIt is " << week[i] << "\nThe bus went through the\n" << stations[j] << "_station and it took " << people << " passangers because it is a TERMINAL STATION."; 
          }
        } 
      } 
    }// END of main while loop
- std::cout <<"\n\n\n[_The bus had total " << total_people << " passangers from 9/7 to 15/7 of the Summer 2018_]";
+ std::cout << "\n\n\n[_The bus had total " << total_people << " passangers from 9/7 to 15/7 of the Summer 2018_]";
 
 return 0;
 }// END of MAIN
